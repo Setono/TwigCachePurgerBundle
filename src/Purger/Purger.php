54eb69e615acbd78b2678ec79fd9c3b2e5d2c4a3
Joachim Løvgaard <joachim@loevgaard.dk>
2022-05-23 09:14:43 +0200

    Add apc invalidation

--- src/Purger/Purger.php
+++ src/Purger/Purger.php
@@ -22,8 +22,8 @@ final class Purger implements PurgerInterface
     public function purge(string $name): void
     {
         /** @psalm-suppress InternalMethod */
-        $cls = new \ReflectionClass($this->twig->load($name)->unwrap());
-        $filename = $cls->getFileName();
+        $reflectionClass = new \ReflectionClass($this->twig->load($name)->unwrap());
+        $filename = $reflectionClass->getFileName();
         if (false === $filename) {
             return;
         }
@@ -31,6 +31,9 @@ final class Purger implements PurgerInterface
         // this bytecode invalidation has been taken from \Twig\Cache\FilesystemCache
         if (\function_exists('opcache_invalidate') && filter_var(ini_get('opcache.enable'), \FILTER_VALIDATE_BOOLEAN)) {
             @opcache_invalidate($filename, true);
+        } elseif (\function_exists('apc_compile_file')) {
+            /** @psalm-suppress UnusedFunctionCall */
+            apc_compile_file($filename);
         }
 
         $this->filesystem->remove($filename);
